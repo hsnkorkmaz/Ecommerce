69682f87a64ff159d67cd0f41a1dbc2e1a602feb
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-04 21:44:52 +0100

    added border color and hover effect

--- client/src/components/product/Product.js
+++ client/src/components/product/Product.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Product = () => {
     return (
-        <div className="mx-2 w-48 lg:mb-6 mb-6">
-            <div className="bg-white flex items-center justify-center rounded-md">
+        <div className="mx-2 w-48 lg:mb-6 mb-6 hover:border-pink-600 hover:shadow-md bg-white rounded-md border-t-2 border-green-200">
+            <div className="flex items-center justify-center ">
                 <img className="w-36" src="https://assets.icanet.se/t_product_medium_v1,f_auto/7300206787006.jpg"/>
             </div>
-            <div className="bg-white rounded-b-md">
+            <div className="rounded-b-md">
                 <div className="p-4">
                     <div className="flex flex-col items-center text-center">
                         <h3 className="text-lg font-thin">Falukorv Klassiker 800g Scan</h3>
