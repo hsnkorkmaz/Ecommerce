ddc7b9cd397826ddfc719daa4b4a072f984b932a
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-06 13:32:29 +0100

    added mobile menu for categories

--- client/src/components/product/ProductList.js
+++ client/src/components/product/ProductList.js
@@ -3,28 +3,45 @@ import CategoryLink from './CategoryLink'
 import Product from './Product'
 
 const ProductList = () => {
+
+    const [isLeftOpen, setLeftOpen] = React.useState(false)
+
+    const mobileNavClass = "transform top-0 left-0 w-64 bg-white fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30 px-3 md:hidden z-50";
+
+
     return (
         <div>
+            <div onClick={() => setLeftOpen(!isLeftOpen)} className="md:hidden">
+                <h1>OPEN</h1>
+            </div>
+            <div className={`transform top-0 left-0 w-full fixed h-full bg-gray-800 opacity-50 z-40 ${!isLeftOpen ? "hidden" : ""}`} onClick={() => setLeftOpen(!isLeftOpen)}>
+
+            </div>
             <div className="container mx-auto flex">
-             
-                <div className="min-w-min mr-4 hidden md:block">
-                    <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-                        Categories
-                    </h1>
+                <aside className={isLeftOpen ? mobileNavClass : "min-w-min mr-4 mt-3 hidden md:block"}>
+                    <div className="flex items-center">
+                        <h1 className="w-full my-2 sm:text-xl md:text-5xl font-bold leading-tight text-center text-gray-800">
+                            Categories
+                        </h1>
+                    {/*     <div className="mr-5 md:hidden"  onClick={() => setLeftOpen(!isLeftOpen)}>
+                            X
+                        </div> */}
+                    </div>
+
                     <div className="w-full mb-4">
                         <div className="h-1 mx-auto w-64 opacity-25 my-0 py-0 rounded-t" style={{ background: "linear-gradient(90deg, #d53369 0%, #daae51 100%)" }}></div>
                     </div>
-                    <div className="mt-10">
+                    <div className="md:mt-10">
                         <ul>
                             <li>
-                               <CategoryLink category="Kött" subCategories={[{name:"Sub1"}, {name: "Sub2"}, {name: "Sub3"}, {name: "Sub4"}]} />
+                                <CategoryLink category="Kött" subCategories={[{ name: "Sub1" }, { name: "Sub2" }, { name: "Sub3" }, { name: "Sub4" }]} />
                             </li>
                             <li>
-                               <CategoryLink category="Fisk" subCategories={[{name:"Sub11"}, {name: "Sub22"}]}/>
+                                <CategoryLink category="Fisk" subCategories={[{ name: "Sub11" }, { name: "Sub22" }]} />
                             </li>
                         </ul>
                     </div>
-                </div>
+                </aside>
                 <div className="w-full">
                     <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
                         Products
