77e42440087f02d5c6ac77dfb1e5a6fce2def5f9
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-07 22:18:30 +0100

    added user icon

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -22,6 +22,14 @@ const Header = () => {
                     </div>
                     <div className="flex">
                         <button
+                            className="text-white mr-2 bg-green-400 rounded-full cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent bg-transparent block lg:hidden outline-none focus:outline-none"
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}>
+                            <svg viewBox="0 0 512 512" enable-background="new 0 0 512 512" className="w-6 h-6 fill-current text-white">
+                                <path d="M454.426,392.582c-5.439-16.32-15.298-32.782-29.839-42.362c-27.979-18.572-60.578-28.479-92.099-39.085  c-7.604-2.664-15.33-5.568-22.279-9.7c-6.204-3.686-8.533-11.246-9.974-17.886c-0.636-3.512-1.026-7.116-1.228-10.661  c22.857-31.267,38.019-82.295,38.019-124.136c0-65.298-36.896-83.495-82.402-83.495c-45.515,0-82.403,18.17-82.403,83.468  c0,43.338,16.255,96.5,40.489,127.383c-0.221,2.438-0.511,4.876-0.95,7.303c-1.444,6.639-3.77,14.058-9.97,17.743  c-6.957,4.133-14.682,6.756-22.287,9.42c-31.521,10.605-64.119,19.957-92.091,38.529c-14.549,9.58-24.403,27.159-29.838,43.479  c-5.597,16.938-7.886,37.917-7.541,54.917h205.958h205.974C462.313,430.5,460.019,409.521,454.426,392.582z" />
+                            </svg>
+                        </button>
+                        <button
                             className="text-white bg-green-400 rounded-full cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent bg-transparent block lg:hidden outline-none focus:outline-none"
                             type="button"
                             onClick={() => setNavbarOpen(!navbarOpen)}>
@@ -65,6 +73,17 @@ const Header = () => {
                                 to="/contact" onClick={() => setNavbarOpen(!navbarOpen)}><span className="ml-2">Contact</span></Link>
                         </li>
                     </ul>
+                </div>
+                <div className="hidden lg:block">
+                    <div className="flex items-center justify-center text-white bg-green-600 rounded-full cursor-pointer px-3 py-2 lg:static mr-3">
+                        <button
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}>
+                             <svg viewBox="0 0 512 512" enable-background="new 0 0 512 512" className="w-6 h-6 fill-current text-white">
+                                <path d="M454.426,392.582c-5.439-16.32-15.298-32.782-29.839-42.362c-27.979-18.572-60.578-28.479-92.099-39.085  c-7.604-2.664-15.33-5.568-22.279-9.7c-6.204-3.686-8.533-11.246-9.974-17.886c-0.636-3.512-1.026-7.116-1.228-10.661  c22.857-31.267,38.019-82.295,38.019-124.136c0-65.298-36.896-83.495-82.402-83.495c-45.515,0-82.403,18.17-82.403,83.468  c0,43.338,16.255,96.5,40.489,127.383c-0.221,2.438-0.511,4.876-0.95,7.303c-1.444,6.639-3.77,14.058-9.97,17.743  c-6.957,4.133-14.682,6.756-22.287,9.42c-31.521,10.605-64.119,19.957-92.091,38.529c-14.549,9.58-24.403,27.159-29.838,43.479  c-5.597,16.938-7.886,37.917-7.541,54.917h205.958h205.974C462.313,430.5,460.019,409.521,454.426,392.582z" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
                 <div className="hidden lg:block">
                     <div className="flex items-center justify-center text-white bg-green-600 rounded-full cursor-pointer px-3 py-2 lg:static">
