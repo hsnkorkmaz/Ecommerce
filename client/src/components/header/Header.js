fceac13590b463bd50fa42d968890de48a097608
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-07 21:09:26 +0100

    added shopping cart icon

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -17,7 +17,19 @@ const Header = () => {
                             className="bg-white outline-none text-sm focus:outline-none rounded-3xl px-5 ml-4" /> */}
 
                     </div>
+                    <div>
+
+                    </div>
                     <div className="flex">
+                        <button
+                            className="text-white bg-green-400 rounded-full cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent bg-transparent block lg:hidden outline-none focus:outline-none"
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}>
+                            <svg viewBox="3.999999761581421 3.0000805854797363 24.003000259399414 21.999919891357422" className="h-5 w-5 fill-current">
+                                <path d="M19.933 22.003h.5a.254.254 0 00.25-.222l1.318-7.496A.252.252 0 0021.75 14h-.5a.253.253 0 00-.251.22l-1.317 7.498a.253.253 0 00.25.285m-4.423-7.75v7.496c0 .14.113.254.253.254h.494a.254.254 0 00.253-.254v-7.496a.253.253 0 00-.253-.253h-.494a.253.253 0 00-.253.253M10.756 14h-.5a.254.254 0 00-.251.285l1.346 7.497a.253.253 0 00.25.22h.497c.152 0 .27-.133.25-.284l-1.341-7.497a.253.253 0 00-.251-.221m12.786 9H8.465l-1.29-11h17.656l-1.289 11zm4.201-13H17.297l-5.63-6.904a.263.263 0 00-.369-.037l-1.144.933a.26.26 0 00-.037.368L14.716 10H4.257a.254.254 0 00-.254.254v.993c0 .002-.003.003-.003.006v.494c0 .14.114.253.253.253H5l1.712 12.782a.257.257 0 00.254.218h18.075a.257.257 0 00.253-.218L27.007 12h.736a.26.26 0 00.26-.26v-1.48a.26.26 0 00-.26-.26z">
+                                </path>
+                            </svg>
+                        </button>
                         <button
                             className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
                             type="button"
@@ -28,30 +40,44 @@ const Header = () => {
                             </svg>
                         </button>
                     </div>
+
                 </div>
                 <div className={"lg:flex flex-grow items-center" + (navbarOpen ? " flex" : " hidden")}>
                     <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-                        <li className="nav-item">
+                        <li>
                             <Link
                                 className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:opacity-75"
                                 to="/" onClick={() => setNavbarOpen(!navbarOpen)}><span className="ml-2">Main Page</span></Link>
                         </li>
-                        <li className="nav-item">
+                        <li>
                             <Link
                                 className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:opacity-75"
                                 to="/about" onClick={() => setNavbarOpen(!navbarOpen)}><span className="ml-2">About</span></Link>
                         </li>
-                        <li className="nav-item">
+                        <li>
                             <Link
                                 className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:opacity-75"
                                 to="/products" onClick={() => setNavbarOpen(!navbarOpen)}><span className="ml-2">Products</span></Link>
                         </li>
-                        <li className="nav-item">
+                        <li>
                             <Link
                                 className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:opacity-75"
                                 to="/contact" onClick={() => setNavbarOpen(!navbarOpen)}><span className="ml-2">Contact</span></Link>
                         </li>
                     </ul>
+                </div>
+                <div className="hidden lg:block">
+                    <div className="flex items-center justify-center text-white bg-green-600 rounded-full cursor-pointer px-3 py-2 lg:static">
+                        <button
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}>
+                            <svg viewBox="3.999999761581421 3.0000805854797363 24.003000259399414 21.999919891357422" className="h-5 w-5 fill-current">
+                                <path d="M19.933 22.003h.5a.254.254 0 00.25-.222l1.318-7.496A.252.252 0 0021.75 14h-.5a.253.253 0 00-.251.22l-1.317 7.498a.253.253 0 00.25.285m-4.423-7.75v7.496c0 .14.113.254.253.254h.494a.254.254 0 00.253-.254v-7.496a.253.253 0 00-.253-.253h-.494a.253.253 0 00-.253.253M10.756 14h-.5a.254.254 0 00-.251.285l1.346 7.497a.253.253 0 00.25.22h.497c.152 0 .27-.133.25-.284l-1.341-7.497a.253.253 0 00-.251-.221m12.786 9H8.465l-1.29-11h17.656l-1.289 11zm4.201-13H17.297l-5.63-6.904a.263.263 0 00-.369-.037l-1.144.933a.26.26 0 00-.037.368L14.716 10H4.257a.254.254 0 00-.254.254v.993c0 .002-.003.003-.003.006v.494c0 .14.114.253.253.253H5l1.712 12.782a.257.257 0 00.254.218h18.075a.257.257 0 00.253-.218L27.007 12h.736a.26.26 0 00.26-.26v-1.48a.26.26 0 00-.26-.26z">
+                                </path>
+                            </svg>
+                        </button>
+                        <span className="text-sm font-medium ml-2">425,25 kr</span>
+                    </div>
                 </div>
             </div>
         </nav>
