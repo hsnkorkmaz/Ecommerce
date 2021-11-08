b3e3d39e91f264d5fb3580c21924a52d448670cc
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-08 10:44:44 +0100

    added search and sort feature

--- client/src/components/product/ProductList.js
+++ client/src/components/product/ProductList.js
@@ -11,19 +11,7 @@ const ProductList = () => {
 
     return (
         <div>
-            <div className="flex justify-evenly md:hidden">
-                <div onClick={() => setLeftOpen(!isLeftOpen)} className="bg-green-600 text-white rounded-2xl p-2 ml-5">
-                    <h1>Categories</h1>
-                </div>
-                <div onClick={() => setLeftOpen(!isLeftOpen)} className="bg-green-600 text-white rounded-2xl p-2 mr-5">
-                    <h1>Filter</h1>
-                </div>
-                <div onClick={() => setLeftOpen(!isLeftOpen)} className="bg-green-600 text-white rounded-2xl p-2 mr-5">
-                    <h1>Sort</h1>
-                </div>
-            </div>
             <div className={`transform top-0 left-0 w-full fixed h-full bg-gray-800 opacity-50 z-40 ${!isLeftOpen ? "hidden" : ""}`} onClick={() => setLeftOpen(!isLeftOpen)}>
-
             </div>
             <div className="container mx-auto flex">
                 <aside className={isLeftOpen ? mobileNavClass : "min-w-min mr-4 mt-3 hidden md:block"}>
@@ -56,6 +44,23 @@ const ProductList = () => {
                     </h1>
                     <div className="w-full mb-4">
                         <div className="h-1 mx-auto w-64 opacity-25 my-0 py-0 rounded-t" style={{ background: "linear-gradient(90deg, #d53369 0%, #daae51 100%)" }}></div>
+                    </div>
+                    <div className="flex flex-wrap justify-start md:justify-between ml-5 lg:ml-0">
+                        <div onClick={() => setLeftOpen(!isLeftOpen)} 
+                        className="bg-green-400 text-white rounded-md p-2 mr-5 w-full md:hidden">
+                            <h1>Categories</h1>
+                        </div>
+                        <div className=" p-2 mr-5 flex items-center w-full lg:w-auto">
+                            <span className="w-16">Search:</span>
+                            <input className="border rounded-md p-2 w-full lg:w-auto focus:outline-none" type="text" placeholder="Search for item" />
+                        </div>
+                        <div className="p-2 mr-5 flex items-center w-full lg:w-auto">
+                            <span className="w-16">Sort:</span>
+                            <select className="border rounded-md p-2 text-gray-400 focus:text-gray-600 w-full lg:w-auto">
+                                <option value="1">Price: Low to High</option>
+                                <option value="2">Price: High to Low</option>
+                            </select>
+                        </div>
                     </div>
                     <div>
                         <div className="mx-auto container py-8">
