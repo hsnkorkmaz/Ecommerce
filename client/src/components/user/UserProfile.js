1a6f5bb2406256d2f85047a0203115dfb9178afe
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-11 21:11:01 +0100

    user data taken from api with jwt cookie

--- client/src/components/user/UserProfile.js
+++ client/src/components/user/UserProfile.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios'
+import { authApi } from '../../api/ApiConfig'
 
 const UserProfile = () => {
+
+    const [user, setUser] = React.useState({})
+
+    useEffect(() => {
+        axios.get(authApi + 'user',
+        { withCredentials: true })
+            .then(function (response) {
+                setUser(response.data)
+                console.log(response);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [])
+
     return (
         <div>
            <h1>User Profile</h1>
-           <h3>Name</h3>
-           <h3>Email</h3>
+           <h3>Name: {user?.name}</h3>
+           <h3>Email: {user?.email}</h3>
            <h3>Orders etc</h3>
         </div>
     )
