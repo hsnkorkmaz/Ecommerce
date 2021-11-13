2625130b51e149a1f859fcac9099c91786d39a25
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-13 11:58:55 +0100

    added logout

--- client/src/components/user/UserProfile.js
+++ client/src/components/user/UserProfile.js
@@ -1,29 +1,61 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import axios from 'axios'
 import { authApi } from '../../api/ApiConfig'
+import UserContext from '../../context/UserContext'
+import { Navigate } from 'react-router-dom'
 
 const UserProfile = () => {
-
-    const [user, setUser] = React.useState({})
+    const [navigate, setNavigate] = React.useState(false)
+    const [currentUser, setCurrentUser] = useState();
+    const { user, setUser } = useContext(UserContext);
 
     useEffect(() => {
-        axios.get(authApi + 'user',
-        { withCredentials: true })
+        if (user === null) {
+            setNavigate(true);
+        } else {
+            axios.get(authApi + 'user',
+                { withCredentials: true })
+                .then(function (response) {
+                    setCurrentUser(response.data);
+                })
+                .catch(err => {
+                    setNavigate(true);
+                });
+        }
+    }, []);
+
+    const logout = () => {
+        axios.get(authApi + 'logout',
+            { withCredentials: true })
             .then(function (response) {
-                setUser(response.data)
-                console.log(response);
+                setUser(null);
+                setNavigate(true);
             })
             .catch(err => {
-                console.log(err);
+                setNavigate(true);
             });
-    }, [])
+    }
+
+    if (navigate) {
+        return <Navigate to='/login' />
+    }
 
     return (
         <div>
-           <h1>User Profile</h1>
-           <h3>Name: {user?.name}</h3>
-           <h3>Email: {user?.email}</h3>
-           <h3>Orders etc</h3>
+
+            <button
+                className="text-white mr-2 bg-green-400 rounded-full cursor-pointer text-l leading-none px-3 py-1 border border-solid border-transparent bg-transparent block lg:hidden outline-none focus:outline-none"
+                onClick={() => logout()}>
+                Logout
+            </button>
+
+
+
+
+            <h1>User Profile</h1>
+            <h3>Name: {currentUser?.name}</h3>
+            <h3>Email: {currentUser?.email}</h3>
+            <h3>Orders etc</h3>
         </div>
     )
 }
