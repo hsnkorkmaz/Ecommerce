f973bccd9f4836b7e2228c2082c64a136c6e8a85
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-14 12:34:56 +0100

    added ica controller for test purposes

---
+++ api/Controllers/IcaController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Entities;
+using api.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IcaController : Controller
+    {
+        private readonly IcaApiService _ica;
+
+        public IcaController(IcaApiService ica)
+        {
+            _ica = ica;
+        }
+
+        [HttpGet("insertAllIcaData")]
+        public async Task<ActionResult> InsertAllData()
+        {
+            await _ica.GetAllIcaData();
+            return Ok();
+        }
+
+        //[HttpGet("getAllData")]
+        //public async Task<ActionResult<List<Category>>> GetAllData()
+        //{
+
+
+        //    //returns categories with subcategories
+
+        //    //var categories = _context.Categories.Where(x => x.CategoryId == null)
+        //    //    .Include(x => x.ChildCategories)
+        //    //    .ThenInclude(x => x.ChildCategories)
+        //    //    .ToList();
+
+
+        //    //var all = await _context.Categories.Where(x=> x.CategoryId == null).Include(x=> x.ChildCategories).ToListAsync();
+        //    //var all = await _context.Categories.Where(x => x.CategoryId == null).Include(x=> x.ChildCategories).ThenInclude().ToListAsync();
+            
+
+
+        //    return Ok();
+        //}
+    }
+}
