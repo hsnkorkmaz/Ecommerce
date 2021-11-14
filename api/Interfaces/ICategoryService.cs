75436c4a3423296ee4dd2b5f4b888de79ea6c178
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-14 13:45:34 +0100

    added category service

---
+++ api/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Entities;
+
+namespace api.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> GetAll();
+    }
+}
---
+++ api/Services/CategoryService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos;
+using api.Entities;
+using api.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly SqlContext _context;
+
+        public CategoryService(SqlContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<Category>> GetAll()
+        {
+            var categories = _context.Categories.Where(x => x.CategoryId == null)
+                .Include(x => x.ChildCategories)
+                .ThenInclude(x => x.ChildCategories)
+                .ToListAsync();
+
+            return categories;
+        }
+    }
+}
