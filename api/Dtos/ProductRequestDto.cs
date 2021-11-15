c9ad658a950aea4979f1a4967edc554812665d12
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-15 13:28:47 +0100

    added product dtos

---
+++ api/Dtos/ProductRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos
+{
+    public class ProductRequestDto
+    {
+        public List<int> CategoryIds { get; set; }
+        public List<int> ProductIds { get; set; }
+        public string Name { get; set; }
+        public int Skip { get; set; }
+        public int Take { get; set; }
+        public string OrderType { get; set; }
+    }
+}
