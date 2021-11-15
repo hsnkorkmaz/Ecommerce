ee7d9477c660a46b1d81bdd8a028b3ff05809530
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-15 13:29:47 +0100

    added productController

---
+++ api/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos;
+using api.Interfaces;
+using AutoMapper;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+        
+        [HttpPost("getProducts")]
+        public async Task<ActionResult> GetProducts(ProductRequestDto request)
+        {
+            var products = await _productService.GetWithDto(request);
+            var productsDto = _mapper.Map<List<ProductDto>>(products);
+            return Ok(productsDto);
+        }
+    }
+}
