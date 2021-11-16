425707940450cc1d0385fab19793baf40e43478b
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-16 22:00:26 +0100

    added order register dtos

---
+++ api/Dtos/OrderRegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos
+{
+    public class OrderRegisterDto
+    {
+        public string DeliveryAddress { get; set; }
+        public List<OrderRegisterItemDto> ItemList { get; set; }
+    }
+
+}
