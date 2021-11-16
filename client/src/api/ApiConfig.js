90323969dc754bfbd36f748a0d3d97def84929ab
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-16 19:11:34 +0100

    added order endpoint

--- client/src/api/ApiConfig.js
+++ client/src/api/ApiConfig.js
@@ -1,3 +1,4 @@
 export const authApi = "https://localhost:44389/api/Auth/";
 export const categoryApi = "https://localhost:44389/api/Category/";
 export const productApi = "https://localhost:44389/api/Product/";
+export const orderApi = "https://localhost:44389/api/Order/";
