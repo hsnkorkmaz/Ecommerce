da1cc87dee1f16ee14947b0bf4c99abe3d0c8e24
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-19 20:03:25 +0100

    added order dtos

---
+++ api/Dtos/OrderDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Entities;
+
+namespace api.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public List<OrderProductDto> Products { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool IsDelivered { get; set; } = false;
+        public DateTime? DeliveryDate { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
---
+++ api/Dtos/OrderProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos
+{
+    public class OrderProductDto
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public ProductDto Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
