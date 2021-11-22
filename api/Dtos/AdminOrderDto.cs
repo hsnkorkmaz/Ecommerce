ccbbd4da7b0430656518f320db8550aecfa8b474
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-23 00:12:46 +0100

    added AdminOrderDto

---
+++ api/Dtos/AdminOrderDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos
+{
+    public class AdminOrderDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool IsDelivered { get; set; } = false;
+        public DateTime? DeliveryDate { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
