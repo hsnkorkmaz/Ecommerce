ba4a419f9e57e20c03f3548c04ac88dfaed768f9
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-11-23 22:17:39 +0100

    added product crud dtos

---
+++ api/Dtos/AdminInsertProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos
+{
+    public class AdminInsertProductDto
+    {
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public List<int> Categories { get; set; }
+    }
+}
---
+++ api/Dtos/AdminUpdateProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos
+{
+    public class AdminUpdateProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public List<int> Categories { get; set; }
+    }
+}
